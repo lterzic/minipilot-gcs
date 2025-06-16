7f61bec93f0bb83295bb2923200583243dc57af8
Luka Terzic <lukaterzicbg@gmail.com>
2025-06-16 18:00:38 +0200

    reworked backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,33 +1,51 @@
-import { pb } from "@minipilot-gcs/proto";
-import { SimLink } from "./link/SimLink.js";
+import { createSocket } from "dgram";
+import { WebSocketServer } from "ws";
 
-const simLink = new SimLink("127.0.0.1", 25564, 25565, 0);
+const SIM_ADDRESS = "127.0.0.1";
+const SIM_PORT_TELEMETRY = 25565;
+const SIM_PORT_COMMAND = 25564;
 
-simLink.onReceiveTelemetry((telemetry) => {
-    console.log(JSON.stringify(telemetry, null, 4));
+const commandSocket = createSocket("udp4");
+const telemetrySocket = createSocket("udp4");
+const wss = new WebSocketServer({port: 25566});
+
+wss.on('connection', (ws) => {
+    console.log("New WS connection...");
+
+    ws.on('error', console.error);
+
+    // Assuming that only command messages are received from clients
+    ws.on('message', (data) => {
+        commandSocket.send(data as Buffer, SIM_PORT_COMMAND, SIM_ADDRESS);
+    });
+
+    ws.on('close', () => {
+        console.log("WS connection closed...");
+    });
 });
 
-setTimeout(() => {
-    const command: pb.mp.Command = {
-        copter: {
-            setAngularVelocity: {
-                thrust: 12,
-                angularVelocity: {
-                    x: 0,
-                    y: 0,
-                    z: 0
-                }
-            }
-        }
-    };
-    simLink.sendCommand(command);
-    console.log("Sent command");
-}, 1000 * 20);
+telemetrySocket.on("message", (msg, rinfo) => {
+    // Assuming that client expects only telemetry messages for now
+    wss.clients.forEach((client) => {
+        client.send(msg);
+    });
+});
 
-process.on('SIGINT', () => {
-    console.log('Received SIGINT. Closing the link...');
+telemetrySocket.bind(SIM_PORT_TELEMETRY, SIM_ADDRESS);
 
-    simLink.close().then(() => {
-        process.exit(0);
-    })
+console.log("Simulator link started...");
+
+process.on("SIGINT", () => {
+    console.log("\nClosing the simulator link...");
+    
+    wss.close(() => {
+        telemetrySocket.close(() => {
+            commandSocket.close(() => {
+                process.exit(0);
+            });
+        });
+    });
+});
+process.on("exit", (code) => {
+    console.log("Exiting...");
 });
