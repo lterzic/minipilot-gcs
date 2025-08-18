a081bf35fec11c5a160147a31c2d6ace3a0f7520
Luka Terzic <lukaterzicbg@gmail.com>
2025-08-18 23:26:32 +0200

    single socket link and mock commands

--- packages/sim-gateway/src/index.ts
+++ packages/sim-gateway/src/index.ts
@@ -1,25 +1,26 @@
 import { createSocket } from "dgram";
 import { WebSocketServer } from "ws";
+import { runCommandSequence } from "./mock-command.js";
+import { pb } from "@minipilot-gcs/proto";
 
 // Has to match with the minipilot-sim udp ports
 const SIM_ADDRESS = "127.0.0.1";
 const SIM_UPLINK_PORT = 25564;
 const SIM_DOWNLINK_PORT = 25565;
 
-// Sockets for the simulator connection
-const uplinkSocket = createSocket("udp4");
-const downlinkSocket = createSocket("udp4");
+// Socket for the simulator connection
+const linkSocket = createSocket("udp4");
 // This is the socket for the frontend connection
 const wss = new WebSocketServer({port: 25566});
 
 wss.on('connection', (ws) => {
-    console.log("New WS connection...");
+    console.log("Connection from: %s", ws.url);
 
     ws.on('error', console.error);
 
     // Pass the message from the frontend to the simulator
     ws.on('message', (data) => {
-        uplinkSocket.send(data as Buffer, SIM_UPLINK_PORT, SIM_ADDRESS);
+        linkSocket.send(data as Buffer, SIM_UPLINK_PORT, SIM_ADDRESS);
     });
 
     ws.on('close', () => {
@@ -27,24 +28,40 @@ wss.on('connection', (ws) => {
     });
 });
 
-downlinkSocket.on("message", (msg, rinfo) => {
+linkSocket.on("message", (msg, rinfo) => {
     // Pass downlink messages from the simulator to the frontend
     wss.clients.forEach((client) => {
         client.send(msg);
     });
+    console.log(JSON.stringify(
+        pb.mp.DownlinkMessage.decode(msg).telemetry,
+        (key, v) => {
+            if (typeof v === "number")
+                return v.toPrecision(3);
+            return v;
+        },
+        2
+    ));
 });
-downlinkSocket.bind(SIM_DOWNLINK_PORT, SIM_ADDRESS);
+linkSocket.bind(SIM_DOWNLINK_PORT, SIM_ADDRESS);
 
 console.log("Simulator link started...");
+
+let uplinkCounter = 0;
+runCommandSequence((cmd) => {
+    linkSocket.send(pb.mp.UplinkMessage.encode({
+        messageId: uplinkCounter++,
+        command: cmd
+    }).finish(), SIM_UPLINK_PORT, SIM_ADDRESS);
+    console.log("Command sent: %s", cmd);
+});
 
 process.on("SIGINT", () => {
     console.log("\nClosing the simulator link...");
     
     wss.close(() => {
-        downlinkSocket.close(() => {
-            uplinkSocket.close(() => {
-                process.exit(0);
-            });
+        linkSocket.close(() => {
+            process.exit(0);
         });
     });
 });
